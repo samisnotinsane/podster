70e12ad5e5d0b662127c0fb614bd6d7ac9f6fb81
samisnotinsane <sameen11@hotmail.com>
2020-02-24 19:37:28 +0000

    Reformatting src to PEP8 conventions

--- web-app/data.py
+++ web-app/data.py
@@ -1,6 +1,4 @@
 from pymongo import MongoClient
-from bson.json_util import dumps
-from bson.json_util import loads
 
 client = MongoClient(port=27017)
 db = client.podster
@@ -9,6 +7,8 @@ db = client.podster
 Retrieves podcasts from MongoDB and returns them 
 as a list. 
 """
+
+
 def get_podcasts():
     shows_list = list()
     for show in db.shows.find():
