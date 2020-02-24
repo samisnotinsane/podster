d9da8598f0b4529cd07199e9770c5be94bf31809
samisnotinsane <sameen11@hotmail.com>
2020-02-24 19:36:43 +0000

    Reformatting src to PEP8 conventions

--- web-app/srv.py
+++ web-app/srv.py
@@ -3,11 +3,13 @@ from data import get_podcasts
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route('/api/podcasts')
 def get_podcast():
     podcasts = get_podcasts()
-    return jsonify({'podcasts' : podcasts})
+    return jsonify({'podcasts': podcasts})
