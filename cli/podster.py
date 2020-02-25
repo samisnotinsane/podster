3c97e5bbcc83e419f96dfa0efa6d509d61d0eebe
samisnotinsane <sameen11@hotmail.com>
2020-02-25 22:01:44 +0000

    Adding click scaffold for cache cmd

--- cli/podster.py
+++ cli/podster.py
@@ -37,5 +37,21 @@ def source(scope, add, purge, view, url):
         click.echo('Viewing RSS URL cache')
 
 
+@cli.command()
+@cli.option('--create', is_flag=True, help='Download RSS feeds using RSS URLs from cache.')
+@cli.option('--update', is_flag=True, help='Overwrite previously downloaded RSS feeds with new data.')
+@cli.option('--purge', is_flag=True, help='Delete stored RSS feeds.')
+@cli.option('--view', is_flag=True, help='View stored RSS feeds.')
+@pass_scope
+def cache(create, update, purge, view):
+    pass
+
+
+@cli.command()
+@pass_scope
+def store():
+    pass
+
+
 if __name__ == '__main__':
     cli()
