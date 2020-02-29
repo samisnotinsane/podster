c6afccb8b9ecca016046e8c245292a93c01fe84c
samisnotinsane <sameen11@hotmail.com>
2020-02-29 17:43:53 +0000

    Adding podcast title using data from API

--- web-app/static/app.js
+++ web-app/static/app.js
@@ -7,6 +7,11 @@ $(document).ready(function () {
             $.each(val, function (idx, epi) {
                 console.log('title: ' + epi.title);
                 console.log('description: ' + epi.description);
+                $('.current-events-shows').append(
+                    '<div class="three columns">\n' +
+                    '<a href="/show-detail">' + epi.title + '</a>\n' +
+                    ' </div>'
+                );
             });
         });
     });
