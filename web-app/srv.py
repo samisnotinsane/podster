f52ed378790eb8794ab572602fa191cd3d7a6d8e
samisnotinsane <sameen11@hotmail.com>
2020-02-29 17:02:04 +0000

    Adding show detail page

--- web-app/srv.py
+++ web-app/srv.py
@@ -38,3 +38,8 @@ def show_episodes():
     episodes_list = data.get_episode_dict(title)['episodes']
     app.logger.debug('Found {0} episodes for \'{1}\' in database'.format(len(episodes_list), title))
     return render_template('show-episodes.html', title=title, episodes=episodes_list)
+
+
+@app.route('/show-detail')
+def show_detail():
+    return render_template('show-detail.html')
