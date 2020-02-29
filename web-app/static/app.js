eddf5649a0dfd58916300aa156f651a03de33ba3
samisnotinsane <sameen11@hotmail.com>
2020-02-29 17:33:27 +0000

    Adding jQuery and logging show metadata using /api/podcasts endpoint

--- web-app/static/app.js
+++ web-app/static/app.js
@@ -1,3 +1,13 @@
-$( document ).ready(function() {
-    console.log( "ready!" );
+$(document).ready(function () {
+    console.log("ready!");
+    $.getJSON('/api/podcasts', function (data) {
+        const items = [];
+        $.each(data, function (key, val) {
+            console.log('key: ' + key);
+            $.each(val, function (idx, epi) {
+                console.log('title: ' + epi.title);
+                console.log('description: ' + epi.description);
+            });
+        });
+    });
 });
