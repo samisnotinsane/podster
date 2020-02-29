ba60a86cb440ef854ec859dc91ed6595b40bdbe5
samisnotinsane <sameen11@hotmail.com>
2020-02-29 19:16:02 +0000

    Adding scrollable cards with podcast coverart

--- web-app/static/app.js
+++ web-app/static/app.js
@@ -1,16 +1,17 @@
 $(document).ready(function () {
     console.log("ready!");
     $.getJSON('/api/podcasts', function (data) {
-        const items = [];
         $.each(data, function (key, val) {
             console.log('key: ' + key);
             $.each(val, function (idx, epi) {
                 console.log('title: ' + epi.title);
                 console.log('description: ' + epi.description);
                 $('.current-events-shows').append(
-                    '<div class="three columns">\n' +
-                    '<a href="/show-detail">' + epi.title + '</a>\n' +
-                    ' </div>'
+                    '<div class="card">\n' +
+                        '<a href="/show-detail">' +
+                        '<img src=\" ' + epi.image_url + '\" height="125" width="125" >' +
+                         '</a>' +
+                    '</div>'
                 );
             });
         });
