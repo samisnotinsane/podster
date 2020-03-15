fb42084ffe1ac396f7906d3d0995145def0354f1
samisnotinsane <sameen11@hotmail.com>
2020-03-15 15:27:09 +0000

    Refactoring recently added js code into a function

--- web-app/static/app.js
+++ web-app/static/app.js
@@ -1,26 +1,23 @@
+function initRecentlyAdded(container, items) {
+    $.each(items, function (index, item) {
+        let coverId = 'cover-' + index;
+        container.append(
+            "<div class='show-grid-item'>" +
+            "<a href='/show-detail" + '?title=' + item['title'] + "'>" +
+            "<img id='" + coverId + "' " +
+            "src='" + item['image_url'] + "' " +
+            "alt='" + item['title'] + "' height='150' width='150'/>" +
+            "</a>" +
+            "</div>"
+        );
+    })
+}
+
 $(document).ready(function () {
-    $.ajax({
-        url: '/api/podcasts',
-        cache: false,
-        success: function (result) {
-            let shows = result['podcasts'];
 
-            console.log(shows.length + ' recently added shows');
-
-            for (let i = 0; i < shows.length; i++) {
-                let coverId = 'cover-' + i;
-                $('.show-section').append(
-                    "<div class='show-grid-item'>" +
-                    "<a href='/show-detail" + '?title=' + shows[i]['title'] + "'>" +
-                    "<img id='" + coverId + "' " +
-                    "src='" + shows[i]['image_url'] + "' " +
-                    "alt='" + shows[i]['title'] + "' height='150' width='150'/>" +
-                    "</a>" +
-                    "</div>"
-                );
-            }
-
-        }
+    // Recently Added
+    $.getJSON('/api/podcasts', function (jsonResponse, status) {
+        initRecentlyAdded($('.show-section'), jsonResponse['podcasts']);
     });
 
     const title = $('.show-meta h3').text();
@@ -69,7 +66,7 @@ $(document).ready(function () {
                     .children()
                     .find('a')
                     .attr('href');
-                console.log(link);
+                // console.log(link);
 
                 $('#player-source').attr('src', link);
             });
