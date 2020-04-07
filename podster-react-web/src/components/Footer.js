88894165d2e93d4ba7be3c2b2ba8ddfd9c22a2ae
samisnotinsane <sameen11@hotmail.com>
2020-04-07 18:25:10 +0100

    Adding homepage

---
+++ podster-react-web/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <div>
+            <a>Don't have an account? Sign up now</a>
+        </div>        
+    );
+}
+
+export default Footer;
---
+++ podster-react-web/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <div className="header-container">
+            <h1>Podster</h1>
+            <p>[Logo Here]</p>
+        </div>
+    );
+}
+
+export default Header;
