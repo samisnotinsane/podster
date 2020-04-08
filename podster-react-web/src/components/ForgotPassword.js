b823a0db1bd3e483cb8a7a8c9b1173afc414da65
samisnotinsane <sameen11@hotmail.com>
2020-04-08 15:45:13 +0100

    Adding forgot password CSS for mobile

--- podster-react-web/src/components/ForgotPassword.js
+++ podster-react-web/src/components/ForgotPassword.js
@@ -3,20 +3,26 @@ import { NavLink } from 'react-router-dom';
 
 function ForgotPassword() {
     return (
-        <div className="forgot-password-container">
-            <span>Forgot your password?</span>
-            <br />
-            <span>
-                Let's fix that. Please enter the email you
-                used when you first signed into Podster and we'll 
-                send you a link to reset your password.
-            </span>
-            <form>
-                <label htmlFor="email">Email:</label>
-                <input type="text" id="email" />
-                <button className="button-primary">Next</button>
-            </form>
-            <p>Back to <NavLink to="/signin" className="accent-link">Sign in</NavLink></p>
+        <div className="page">
+            <div className="section header">
+                <span className="brand-tagline">Forgot your password?</span>
+            </div>
+            <div className="section header">
+                <span>
+                    Let's fix that. Please enter the email you
+                    used when you first signed into Podster and we'll 
+                    send you a link to reset your password.
+                </span>
+            </div>
+            <div className="section sign-in">
+                <form className="form">
+                    <label htmlFor="email">Email:</label>
+                    <input type="text" id="email" />
+                    <button className="button-primary">Next</button>
+                </form>
+                <p>Back to <NavLink to="/signin" className="accent-link">Sign in</NavLink></p>
+            </div>
+            
             <NavLink to="/new-password">New password</NavLink>
         </div>
     );
--- podster-react-web/src/components/Signin.js
+++ podster-react-web/src/components/Signin.js
@@ -5,18 +5,23 @@ function Signin() {
     return (
         <div className="page">
             <div className="section header">
-                <span>Sign In</span>
+                <span className="brand-name">Sign In</span>
             </div>
             <div className="section sign-in">
-                <form>
+                <form className="form">
                     <label htmlFor="email">Email</label>
                     <input type="text" id="email" />
                     <br />
                     <label htmlFor="password">Password</label>
                     <input type="password" id="password" />
                     <br />
-                    <input type="checkbox" name="isRemainSignedIn" value="true" /> Keep me logged in
-                    <button className="button-primary">Let's go!</button>
+                    <div className="form-row">
+                        Keep me logged in
+                        <input type="checkbox" name="isRemainSignedIn" value="true" />
+                    </div>
+                    <div className="form-row">
+                        <button className="button-primary">Let's go!</button>
+                    </div>
                 </form>
             </div>
             
