d3a4047a6ae2fd1bb577feb7327b7f9ddabadc47
samisnotinsane <sameen11@hotmail.com>
2020-04-08 15:56:42 +0100

    Adding new password revised layout

--- podster-react-web/src/components/NewPassword.js
+++ podster-react-web/src/components/NewPassword.js
@@ -2,19 +2,24 @@ import React from 'react';
 
 function NewPassword() {
     return (
-        <div className="new-password-container">
-            <span>Reset your password</span>
-                <br />
-            <span>Enter your new password down below:</span>
-            <form>
-                <label htmlFor="newPassword">New Password</label>
-                <input type="password" id="newPassword" />
-                <br />
-                <label htmlFor="confirmNewPassword">Confirm new password</label>
-                <input type="password" id="confirmNewPassword" />
-                <button className="button-primary">Change Password</button>
-            </form>
-            <span>Tip: Strong passwords use upper and lowercase characters, numbers and symbols.</span>
+        <div className="page">
+            <div className="section header">
+                <span className="brand-name">Reset your password</span>
+                <span className="brand-tagline">Enter your new password down below:</span>
+            </div>
+            <div className="section sign-in">
+                <form className="form">
+                    <label htmlFor="newPassword">Password</label>
+                    <input type="password" id="newPassword" />
+                    <label htmlFor="confirmNewPassword">Confirm new password</label>
+                    <input type="password" id="confirmNewPassword" />
+                    
+                    <div className="form-row">
+                        <button className="button-primary">Change Password</button>
+                    </div>
+                </form>
+                <span>Tip: Strong passwords use upper and lowercase characters, numbers and symbols.</span>
+            </div>
         </div>
     );
 }
