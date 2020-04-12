231c2814b9a362039d55aafa712f9a2cf5ba1982
samisnotinsane <sameen11@hotmail.com>
2020-04-12 09:51:52 +0100

    Adding state and event to forgot and new password

--- podster-react-web/src/components/ForgotPassword.js
+++ podster-react-web/src/components/ForgotPassword.js
@@ -3,31 +3,57 @@ import React from 'react';
 import ButtonPrimary from './ButtonPrimary.js';
 import ButtonInline from './ButtonInline.js';
 
-function ForgotPassword() {
-    return (
-        <div className="page">
-            <div className="section header">
-                <span className="brand-tagline">Forgot your password?</span>
+class ForgotPassword extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+        };
+        this.onEmailChange = this.onEmailChange.bind(this);
+    }
+
+    onEmailChange(event) {
+        this.setState({
+            email: event.target.value,
+        });
+    }
+
+    render () {
+        const {
+            email
+        } = this.state;
+        return (
+            <div className="page">
+                <div className="section header">
+                    <span className="brand-tagline">Forgot your password?</span>
+                </div>
+                <div className="section header">
+                    <span>
+                        Let's fix that. Please enter the email you
+                        used when you first signed into Podster and we'll 
+                        send you a link to reset your password.
+                    </span>
+                </div>
+                <div className="section sign-in">
+                    <form className="form">
+                        <label htmlFor="email">Email:</label>
+                        <input 
+                            id="email" 
+                            type="text"
+                            value={email}
+                            onChange={this.onEmailChange}
+                        />
+                        <br />
+                        <ButtonPrimary linkTo="/new-password">Next</ButtonPrimary>
+                    </form>
+                    <span>Back to <ButtonInline linkTo="/signin">Sign in</ButtonInline></span>
+                </div>
+                <hr />
+                <h3>Debug</h3>
+                <p>Email entered: {email}</p>
             </div>
-            <div className="section header">
-                <span>
-                    Let's fix that. Please enter the email you
-                    used when you first signed into Podster and we'll 
-                    send you a link to reset your password.
-                </span>
-            </div>
-            <div className="section sign-in">
-                <form className="form">
-                    <label htmlFor="email">Email:</label>
-                    <input type="text" id="email" />
-                    <br />
-                    <ButtonPrimary linkTo="/new-password">Next</ButtonPrimary>
-                </form>
-                <span>Back to <ButtonInline linkTo="/signin">Sign in</ButtonInline></span>
-                
-            </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default ForgotPassword;
--- podster-react-web/src/components/NewPassword.js
+++ podster-react-web/src/components/NewPassword.js
@@ -2,29 +2,72 @@ import React from 'react';
 
 import ButtonPrimary from './ButtonPrimary.js';
 
-function NewPassword() {
-    return (
-        <div className="page">
-            <div className="section header">
-                <span className="brand-name">Reset your password</span>
-                <br />
-                <span className="brand-tagline">Enter your new password down below:</span>
+class NewPassword extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            password: '',
+            confirmPassword: '',
+        };
+        this.onPasswordChange = this.onPasswordChange.bind(this);
+        this.onConfirmPasswordChange = this.onConfirmPasswordChange.bind(this);
+    }
+
+    onPasswordChange(event) {
+        this.setState({
+            password: event.target.value,
+        });
+    }
+
+    onConfirmPasswordChange(event) {
+        this.setState({
+            confirmPassword: event.target.value,
+        });
+    }
+
+    render () {
+        const {
+            password,
+            confirmPassword
+        } = this.state;
+
+        return (
+            <div className="page">
+                <div className="section header">
+                    <span className="brand-name">Reset your password</span>
+                    <br />
+                    <span className="brand-tagline">Enter your new password down below:</span>
+                </div>
+                <div className="section sign-in">
+                    <form className="form">
+                        <label htmlFor="newPassword">Password</label>
+                        <input 
+                            id="newPassword" 
+                            type="password"
+                            value={password}
+                            onChange={this.onPasswordChange}
+                        />
+                        <label htmlFor="confirmNewPassword">Confirm new password</label>
+                        <input 
+                            id="confirmNewPassword" 
+                            type="password"
+                            value={confirmPassword}
+                            onChange={this.onConfirmPasswordChange}
+                        />
+                        
+                        <div className="form-row">
+                            <ButtonPrimary>Change Password</ButtonPrimary>
+                        </div>
+                    </form>
+                    <span>Tip: Strong passwords use upper and lowercase characters, numbers and symbols.</span>
+                </div>
+                <hr />
+                <h3>Debug</h3>
+                <p>Password entered: {password}</p>
+                <p>Confirm password entered: {confirmPassword}</p>
             </div>
-            <div className="section sign-in">
-                <form className="form">
-                    <label htmlFor="newPassword">Password</label>
-                    <input type="password" id="newPassword" />
-                    <label htmlFor="confirmNewPassword">Confirm new password</label>
-                    <input type="password" id="confirmNewPassword" />
-                    
-                    <div className="form-row">
-                        <ButtonPrimary>Change Password</ButtonPrimary>
-                    </div>
-                </form>
-                <span>Tip: Strong passwords use upper and lowercase characters, numbers and symbols.</span>
-            </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default NewPassword;
