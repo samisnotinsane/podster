2d16149af1e2b8d4a9acb0bbea2f3ed2c38eb195
samisnotinsane <sameen11@hotmail.com>
2020-04-21 22:59:57 +0100

    Closing dead links

--- podster-react-web/src/components/Home/SignUpLink.js
+++ podster-react-web/src/components/Home/SignUpLink.js
@@ -4,13 +4,20 @@ import Link from "@material-ui/core/Link";
 
 import * as ROUTES  from '../../constants/routes';
 
+const handleClick = () => {
+    alert(
+        `Podster is under development. We cannot accept new sign ups at this moment.`
+    );
+}
+
 const SignUpLink = () => {
     return (
         <Link 
-            href={ROUTES.SIGN_UP} 
+            // href={ROUTES.SIGN_UP} 
             variant="body2" 
             color="secondary"
             underline="hover"
+            onClick={() => {handleClick()}}
         >
             Don't have an account? Sign up now
         </Link>
